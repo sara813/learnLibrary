bc4d204f2b7a80eb5e665f8621e653d9f9a2507f
欧阳睿青 <271105080@qq.com>
2018-10-19 17:50:28 +0800

    管理资源

---
+++ webpackDemo/src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import "./style.css";
+// import myImage from "./photo.png";
+import Data from "./data.xml"
+
+
+function component() {
+    var element = document.createElement("div");
+    element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.classList.add("hello");
+    console.log(Data);
+    return element;
+}
+
+document.body.appendChild(component());
