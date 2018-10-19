6082407a86cb515231cf4425333965f41a7455e8
欧阳睿青 <271105080@qq.com>
2018-10-19 17:57:50 +0800

    清空准备进入管理输出学习

--- webpackDemo/src/index.js
+++ webpackDemo/src/index.js
@@ -1,14 +1,9 @@
 import _ from 'lodash';
-import "./style.css";
-// import myImage from "./photo.png";
-import Data from "./data.xml"
 
 
 function component() {
     var element = document.createElement("div");
     element.innerHTML = _.join(["Hello", "webpack"], " ");
-    element.classList.add("hello");
-    console.log(Data);
     return element;
 }
 
