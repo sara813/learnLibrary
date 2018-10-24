abe5449f3e92298dabe5aa4f5aca40908a37890e
欧阳睿青 <271105080@qq.com>
2018-10-24 15:31:51 +0800

    管理输出

--- webpackDetail/src/index.js
+++ webpackDetail/src/index.js
@@ -1,12 +1,13 @@
 import _ from "lodash";
-import "./style.css";
-import Data from "./data.xml"
+import printMe from "./print.js";
 
 function component() {
     var element = document.createElement("div");
     element.innerHTML = _.join(["Hello", "webpack"], " ");
-    element.classList.add("hello");
-    console.log(Data);
+    var btn = document.createElement("button");
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
--- webpackDetail/webpack.config.js
+++ webpackDetail/webpack.config.js
@@ -1,31 +1,20 @@
 
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: {
+        app: "./src/index.js",
+        print: "./src/print.js"
+    },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "[name].bundle.js"
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ["file-loader"]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: ["csv-loader"]
-            },
-            {
-                test: /\.xml$/,
-                use: ["xml-loader"]
-            }
-        ]
-    }
+    plugins: [
+        new htmlWebpackPlugin({
+            title: "Output Management"
+        })
+    ]
 };
